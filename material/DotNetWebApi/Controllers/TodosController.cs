0d63750abff04ae43f16cd5bfb651dedc6e642ff
Esteban Solano <stvansolano>
2021-03-25 20:45:45 -0600

    Web Api / Controllers

---
+++ material/DotNetWebApi/Controllers/TodosController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DotNetWebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/[controller]")]
+    public class TodosController : Controller
+    {
+        private readonly ILogger<TodosController> _logger;
+
+        public TodosController(ILogger<TodosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+            => Ok(new object[] { 1, "2", DateTime.Now });
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string input)
+        {
+            return Created(nameof(Get), input);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] object input)
+        {
+            return Ok();
+        }
+    }
+}
--- material/DotNetWebApi/Program.cs
+++ material/DotNetWebApi/Program.cs
@@ -1,59 +1,20 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Primitives;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
+using Microsoft.Extensions.Hosting;
 
-WebHost.CreateDefaultBuilder().Configure(app =>
+namespace DotNetWebApi
 {
-    app.UseRouting();
-    app.UseEndpoints(e =>
+    public class Program
     {
-        e.MapGet("/", c => c.Response.WriteAsync("Hello world!"));
-        
-        e.MapGet("hello", context =>
-            context.Response.WriteAsJsonAsync(new { Message = "Hello, visitor"})
-        );
-        
-        e.MapGet("hello/{name}", context => {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-            context.Response.WriteAsync($"Hello, {context.Request.RouteValues["name"]}");
-
-            return System.Threading.Tasks.Task.CompletedTask;
-        });
-
-        e.MapPost("hello", async context => {
-
-            var value = new StringValues(context.Request.Host.Host);
-
-            context.Response.Headers.Add("Host-name", value);
-
-            using var reader = new StreamReader(context.Request.Body, System.Text.Encoding.UTF8);
-            var content = await reader.ReadToEndAsync();
-
-            System.Console.WriteLine(content);
-
-            context.Response.StatusCode = 200;
-        });
-
-        e.MapGet("db/hello", async context => {
-            
-            var adventureWorks = "data source=localhost,1433;initial catalog=Adventureworks;persist security info=True;user id=sa;password=Password.123;MultipleActiveResultSets=True;";
-
-            using (var connection = new SqlConnection(adventureWorks))
-            {
-                SqlCommand command = new SqlCommand("EXEC [dbo].[sp_HelloWorld]", connection);
-                command.Connection.Open();
-                var helloDb = command.ExecuteScalar() as string;
-
-                context.Response.StatusCode = 200;
-
-                await context.Response.WriteAsync(helloDb);
-            }
-        });
-    });
-}).Build().Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
